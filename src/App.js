d0525769fad5cdfe9949783004789ca212fd269b
zz1qwop <84325395+zz1qwop@users.noreply.github.com>
2023-06-08 14:50:17 +0900

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import './App.css';
+import MyCalendarPage from './Pages/MyCalendarPage';
+
+function App() {
+  return (
+    <>
+      <MyCalendarPage />
+    </>
+  );
+}
+
+export default App;
---
+++ src/Pages/MyCalendarPage.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import CalendarBox from '../Components/CalendarBox';
+import Schedule from '../Components/Schedule';
+
+export default function MyCalendarPage() {
+  return (
+    <>
+      <CalendarBox />
+      <Schedule />
+    </>
+  );
+}
